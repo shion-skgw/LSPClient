4e3e8f27025a96dd8396f3aba9993d6b7c961f9e
Shion Sukegawa <sean.skgw@gmail.com>
2020-06-07 08:32:43 +0900

    Initial Commit

---
+++ LSPClient/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LSPClient
+//
+//  Created by Shion on 2020/06/07.
+//  Copyright © 2020 Shion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
