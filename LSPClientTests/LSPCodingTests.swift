f39000fcef0ada56cd862dda205141539e78d3a9
Shion Sukegawa <sean.skgw@gmail.com>
2020-07-31 05:44:18 +0900

    Commit

---
+++ LSPClientTests/LSPCodingTests.swift
@@ -0,0 +1,13 @@
+//
+//  LSPCodingTests.swift
+//  LSPClientTests
+//
+//  Created by Shion on 2020/07/30.
+//  Copyright © 2020 Shion. All rights reserved.
+//
+
+import XCTest
+@testable import LSPClient
+
+class LSPCodingTests: XCTestCase {
+}
