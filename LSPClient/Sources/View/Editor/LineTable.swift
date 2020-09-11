7747bc013eb5c0537590c4a77dfd97d7060c0e7b
Shion Sukegawa <sean.skgw@gmail.com>
2020-09-12 03:35:42 +0900

    Add comment

--- LSPClient/Sources/View/Editor/LineTable.swift
+++ LSPClient/Sources/View/Editor/LineTable.swift
@@ -14,53 +14,80 @@ extension String {
 	}
 }
 
+///
+/// Line range table
+///
 final class LineTable {
+
+	/// Line range table (Key: Zero based line number, Value: Line range)
 	var table: [Int: NSRange]
+	/// Text contents
 	weak var content: NSMutableAttributedString?
+	/// Newline character
 	private let newLine: NSRegularExpression = try! NSRegularExpression(pattern: "\n")
 
-	init(string: NSMutableAttributedString) {
+	///
+	/// Initialize
+	///
+	/// - Parameter content		: Text contents
+	///
+	init(content: NSMutableAttributedString) {
 		self.table = [:]
-		self.content = string
-		update(for: string.string.range)
+		self.content = content
+		update(for: content.string.range)
 	}
 
+	///
+	/// Replaces characters
+	///
+	/// - Parameter range		: Replacement range
+	/// - Parameter str			: Replacement string
+	///
 	func replaceCharacters(in range: NSRange, with str: String) {
 		content?.replaceCharacters(in: range, with: str)
 		update(for: range)
 	}
 
-	func update(for range: NSRange) {
+	///
+	/// Update line range table
+	///
+	/// - Parameter range		: Update range
+	///
+	private func update(for range: NSRange) {
 		guard let string = content?.string else {
 			return
 		}
 
+		// Update start position
 		var testRange: NSRange
 		var lineNumber: Int
-		var previous: Int
+		var previousLineEnd: Int
 		if let lineRange = table.filter({ NSLocationInRange(range.lowerBound, $0.value) }).first {
 			testRange = NSMakeRange(lineRange.value.location, string.range.length - lineRange.value.location)
 			lineNumber = lineRange.key
-			previous = lineRange.value.upperBound
+			previousLineEnd = lineRange.value.upperBound
 		} else {
 			testRange = string.range
 			lineNumber = 0
-			previous = 0
+			previousLineEnd = 0
 		}
 
+		// Create a new line range table
 		var newTable: [Int: NSRange] = [:]
 		newLine.enumerateMatches(in: string, options: [], range: testRange) {
 			(result, _, _) in
-			guard let current = result?.range.upperBound else {
+			guard let currentLineEnd = result?.range.upperBound else {
 				return
 			}
-			newTable[lineNumber] = NSMakeRange(previous, current - previous)
+			newTable[lineNumber] = NSMakeRange(previousLineEnd, currentLineEnd - previousLineEnd)
 			lineNumber += 1
-			previous = current
+			previousLineEnd = currentLineEnd
 		}
-		if previous < string.range.upperBound {
-			newTable[lineNumber] = NSMakeRange(previous, string.range.upperBound - previous)
+		if previousLineEnd < string.range.upperBound {
+			newTable[lineNumber] = NSMakeRange(previousLineEnd, string.range.upperBound - previousLineEnd)
 		}
+
+		// Merge line range table
 		if let minLineNumber = newTable.min(by: { $0.key < $1.key })?.key, 0 < minLineNumber {
 			table = newTable.merging(table.filter({ $0.key < minLineNumber }), uniquingKeysWith: { (new, old) in new })
 		} else {
@@ -68,6 +95,12 @@ final class LineTable {
 		}
 	}
 
+	///
+	/// Returns line range
+	///
+	/// - Parameter range		: NS characters range
+	/// - Returns				: LSP line range
+	///
 	func lineRange(for range: NSRange) -> NSRange? {
 		guard let start = table.filter({ NSLocationInRange(range.lowerBound, $0.value) }).first?.value,
 				let end = table.filter({ NSLocationInRange(range.upperBound, $0.value) }).first?.value else {
@@ -76,6 +109,12 @@ final class LineTable {
 		return NSMakeRange(start.lowerBound, end.upperBound)
 	}
 
+	///
+	/// Returns line range
+	///
+	/// - Parameter range		: LSP characters range
+	/// - Returns				: NS line range
+	///
 	func lineRange(for textRange: TextRange) -> NSRange? {
 		guard let start = table[textRange.start.line],
 				let end = table[textRange.end.line] else {
@@ -84,6 +123,12 @@ final class LineTable {
 		return NSMakeRange(start.lowerBound, end.upperBound)
 	}
 
+	///
+	/// Convert characters range
+	///
+	/// - Parameter textRange	: LSP characters range
+	/// - Returns				: NS characters range
+	///
 	func range(for textRange: TextRange) -> NSRange? {
 		guard let start = table[textRange.start.line],
 				let end = table[textRange.end.line] else {
@@ -94,6 +139,12 @@ final class LineTable {
 		return NSMakeRange(location, length)
 	}
 
+	///
+	/// Convert characters range
+	///
+	/// - Parameter range		: NS characters range
+	/// - Returns				: LSP characters range
+	///
 	func range(for range: NSRange) -> TextRange? {
 		guard let start = position(for: range.lowerBound),
 				let end = position(for: range.upperBound) else {
@@ -102,6 +153,12 @@ final class LineTable {
 		return TextRange(start: start, end: end)
 	}
 
+	///
+	/// Convert characters position
+	///
+	/// - Parameter position	: NS characters position
+	/// - Returns				: LSP characters position
+	///
 	func position(for position: Int) -> TextPosition? {
 		guard let line = table.filter({ NSLocationInRange(position, $0.value) }).first else {
 			return nil
